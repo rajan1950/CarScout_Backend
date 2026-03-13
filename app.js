09de602cefc27f0169c2b51c988bfc2d9d7d2fce
rajan1950 <rajandobariya6@gmail.com>
2026-03-13 11:58:52 +0530

    Add routes for inquiry, admin, message, review, and test drive management

--- app.js
+++ app.js
@@ -17,6 +17,21 @@ app.use("/user",userRoutes);
 const carRoutes = require("./src/routes/CarRoutes");
 app.use("/car",carRoutes);
 
+const inquiryRoutes = require("./src/routes/InquiryRoutes");
+app.use("/inquiry",inquiryRoutes);
+
+const adminRoutes = require("./src/routes/AdminRoutes");
+app.use("/admin",adminRoutes);
+
+const messageRoutes = require("./src/routes/MessageRoutes");
+app.use("/message",messageRoutes);
+
+const reviewRoutes = require("./src/routes/ReviewRoutes");
+app.use("/reviews",reviewRoutes);
+
+const testDriveRoutes = require("./src/routes/TestDriveRoutes");
+app.use("/testdrive",testDriveRoutes);
+
 const PORT= process.env.PORT  
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`); 
