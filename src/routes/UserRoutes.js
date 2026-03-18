717adf637c776d67b8a5e7c36b1edc66e5b3772c
rajan1950 <rajandobariya6@gmail.com>
2026-03-18 23:57:43 +0530

    Add token validation to user routes for enhanced security

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,22 +1,40 @@
 const router = require("express").Router();
 const usercontroller = require("../controller/UserController");
 
+
 // CREATE - POST
+
+
 //localhost:4444/user/register
-router.get("/register", usercontroller.registerUser);
-  
 router.post("/register", usercontroller.registerUser)
 //localhost:4444/user/login
 router.post("/login", usercontroller.loginUser);
+const validateToken = require("../middleware/AuthMiddleware");
 //localhost:4444/user/getallusers
-router.get("/getallusers", usercontroller.getAllUsers);
+router.get("/getallusers", validateToken, usercontroller.getAllUsers);
 //localhost:4444/user/getallusers/1
-router.get("/getallusers/:id", usercontroller.getUserById);
+router.get("/getallusers/:id", validateToken, usercontroller.getUserById);
+//localhost:4444/user/getallusers/1
+router.put("/getallusers/:id", validateToken, usercontroller.updateUser);
+//localhost:4444/user/getallusers/1
+router.delete("/getallusers/:id", validateToken, usercontroller.deleteUser);
 
-router.put("/getallusers/:id", usercontroller.updateUser);
+// {
+// //localhost:4444/user/register
+// router.get("/register", usercontroller.registerUser);
+  
+// router.post("/register", usercontroller.registerUser)
+// //localhost:4444/user/login
+// router.post("/login", usercontroller.loginUser);
+// //localhost:4444/user/getallusers
+// router.get("/getallusers", usercontroller.getAllUsers);
+// //localhost:4444/user/getallusers/1
+// router.get("/getallusers/:id", usercontroller.getUserById);
 
-router.delete("/getallusers/:id", usercontroller.deleteUser);
+// router.put("/getallusers/:id", usercontroller.updateUser);
 
+// router.delete("/getallusers/:id", usercontroller.deleteUser);
 
+// }
 
 module.exports = router;    
