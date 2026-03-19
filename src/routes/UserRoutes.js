7aacd2d58088bc0fbc8d35a7e79880fbc5013316
rajan1950 <rajandobariya6@gmail.com>
2026-03-19 14:07:46 +0530

    Remove commented-out user route definitions for clarity

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -19,22 +19,6 @@ router.put("/getallusers/:id", validateToken, usercontroller.updateUser);
 //localhost:4444/user/getallusers/1
 router.delete("/getallusers/:id", validateToken, usercontroller.deleteUser);
 
-// {
-// //localhost:4444/user/register
-// router.get("/register", usercontroller.registerUser);
-  
-// router.post("/register", usercontroller.registerUser)
-// //localhost:4444/user/login
-// router.post("/login", usercontroller.loginUser);
-// //localhost:4444/user/getallusers
-// router.get("/getallusers", usercontroller.getAllUsers);
-// //localhost:4444/user/getallusers/1
-// router.get("/getallusers/:id", usercontroller.getUserById);
 
-// router.put("/getallusers/:id", usercontroller.updateUser);
-
-// router.delete("/getallusers/:id", usercontroller.deleteUser);
-
-// }
 
 module.exports = router;    
