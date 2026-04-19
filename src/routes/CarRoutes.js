30f26f026f6e0c041c6a5a091333f4f1d2758f96
rajan1950 <rajandobariya6@gmail.com>
2026-04-19 09:07:05 +0530

    Refactor car addition route to support multiple image uploads

--- src/routes/CarRoutes.js
+++ src/routes/CarRoutes.js
@@ -12,7 +12,14 @@ const router = express.Router();
 
 const upload = require("../middleware/UploadMiddleware");
 
-router.post("/add",upload.single("image"), createCar);//localhost:4444/car/add
+router.post(
+  "/add",
+  upload.fields([
+    { name: "image", maxCount: 10 },
+    { name: "images", maxCount: 10 }
+  ]),
+  createCar
+);//localhost:4444/car/add
 router.get("/all", getAllCars);//localhost:4444/car/all
 router.get("/:id", getCarById);//localhost:4444/car/1
 router.put("/:id", updateCar);//localhost:4444/car/1
